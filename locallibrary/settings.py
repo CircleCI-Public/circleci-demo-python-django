8ec140647cba014e5575dba361b746be77454d84
Tom Atkins <tom@keybits.net>
2018-01-16 00:15:35 +0000

    Fix test-results paths

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -150,4 +150,4 @@ STATIC_URL = '/static/'
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
 TEST_OUTPUT_VERBOSE = 2
-TEST_OUTPUT_DIR = 'test-reports'
+TEST_OUTPUT_DIR = 'test-results'
