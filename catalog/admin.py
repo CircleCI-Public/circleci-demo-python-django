ae18f694f9e2877e4b4a9196af0b955b88caaffb
jkzilla <johanna@circleci.com>
2020-09-01 13:38:55 -0700

    trying new import models style3

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from models import models.Genre, models.Book, models.Language, models.BookInstance, models.Author
+from django.db import models
 
 """
 # Minimal registration of Models.
