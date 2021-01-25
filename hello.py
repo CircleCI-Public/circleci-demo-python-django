7edd99cb7f2f75a75d22c5b623163125e464de0c
Henna Abbas <henna@circleci.com>
2021-01-25 12:08:27 -1000

    commit

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
-# def hello():
-#     print("HELLO WORLD")
-#     return "hello"
-print("hello world")
+import requests
+
+payload = {'term': 'honeydew'}
+
+res = requests.get('https://itunes.apple.com/search',
+                   params=payload)
+
+melon_songs = res.json()
+
+num_results = melon_songs['resultCount']
+
+for i in range(num_results):
+    trackName = melon_songs['results'][i].get('trackName')
+    artistName = melon_songs['results'][i].get('artistName')
+    print({trackName})
